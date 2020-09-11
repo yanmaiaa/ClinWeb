69e343273e23e24244a3dd2fa7648ade8547bb42
Joan Pedro <jpos.senai@gmail.com>
2020-09-11 11:16:55 -0300

    test: ensure SignUpController calls handle with an httpRequest

--- backend/src/presentation/controllers/signup.spec.ts
+++ backend/src/presentation/controllers/signup.spec.ts
@@ -2,6 +2,21 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
+  test('Should call handle with httpRequest', () => {
+    const sut = new SignUpController()
+    const handleSpy = jest.spyOn(sut, 'handle')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
