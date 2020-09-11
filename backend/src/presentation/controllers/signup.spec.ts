5f9c89ad27d8295328999a136925154c960d21e2
Joan Pedro <jpos.senai@gmail.com>
2020-09-11 13:24:14 -0300

    refactor: ensure mock EmailValidator with error on jest spyOn

--- backend/src/presentation/controllers/signup.spec.ts
+++ backend/src/presentation/controllers/signup.spec.ts
@@ -253,14 +253,10 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 500 if EmailValidator throws', () => {
-    class EmailValidatorStub implements EmailValidator {
-      isValid = (email: string): boolean => {
-        throw new Error()
-      }
-    }
-    const emailValidatorStub = new EmailValidatorStub()
-    const { fieldValidatorStub } = makeSut()
-    const sut = new SignUpController(fieldValidatorStub, emailValidatorStub)
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
 
     const httpRequest = {
       body: {
