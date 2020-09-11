dc4dc353d57daafbf9a407a422d352fd44ce78ce
Joan Pedro <jpos.senai@gmail.com>
2020-09-11 13:16:05 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- backend/src/presentation/controllers/signup.ts
+++ backend/src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
+import { ServerError } from '../errors/server-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
@@ -13,24 +14,31 @@ export class SignUpController implements Controller {
   ) {}
 
   handle = (httpRequest: HttpRequest): HttpResponse => {
-    const requiredField = ['name', 'email', 'password', 'passwordConfirmation', 'isProfessional']
-    for (const field of requiredField) {
-      if (httpRequest.body[field] === undefined) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredField = ['name', 'email', 'password', 'passwordConfirmation', 'isProfessional']
+      for (const field of requiredField) {
+        if (httpRequest.body[field] === undefined) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      const optionalField = ['professionName']
+      for (const field of optionalField) {
+        if (httpRequest.body.isProfessional && httpRequest.body[field] === undefined) {
+          return badRequest(new InvalidParamError(field))
+        }
+        if (!httpRequest.body.isProfessional && httpRequest.body[field]) {
+          return badRequest(new InvalidParamError('professionName must no be provided'))
+        }
+      }
+      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      if (!isValid) return badRequest(new InvalidParamError('email'))
+      const isNumber = this.fieldValidator.isNumber(httpRequest.body.isProfessional)
+      if (!isNumber) return badRequest(new InvalidParamError('isProfessional'))
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
       }
     }
-    const optionalField = ['professionName']
-    for (const field of optionalField) {
-      if (httpRequest.body.isProfessional && httpRequest.body[field] === undefined) {
-        return badRequest(new InvalidParamError(field))
-      }
-      if (!httpRequest.body.isProfessional && httpRequest.body[field]) {
-        return badRequest(new InvalidParamError('professionName must no be provided'))
-      }
-    }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-    if (!isValid) return badRequest(new InvalidParamError('email'))
-    const isNumber = this.fieldValidator.isNumber(httpRequest.body.isProfessional)
-    if (!isNumber) return badRequest(new InvalidParamError('isProfessional'))
   }
 }
