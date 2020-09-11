475bd76e45c29152f56ca167faf8ea6f226b617a
Joan Pedro <jpos.senai@gmail.com>
2020-09-11 11:35:30 -0300

    feat: ensure SignUpController returns 400 if no professionName is provided

--- backend/src/presentation/controllers/signup.ts
+++ backend/src/presentation/controllers/signup.ts
@@ -5,7 +5,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
   handle = (httpRequest: HttpRequest): HttpResponse => {
-    const requiredField = ['name', 'email', 'password', 'passwordConfirmation', 'isProfessional', 'professionalName']
+    const requiredField = ['name', 'email', 'password', 'passwordConfirmation', 'isProfessional', 'professionName']
     for (const field of requiredField) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
