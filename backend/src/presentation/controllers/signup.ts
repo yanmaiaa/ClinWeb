7d16fa8597d53493cbbc87cdfaab2fbc23935e40
Joan Pedro <jpos.senai@gmail.com>
2020-09-11 11:22:04 -0300

    feat: add professional fields on request

--- backend/src/presentation/controllers/signup.ts
+++ backend/src/presentation/controllers/signup.ts
@@ -5,7 +5,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
   handle = (httpRequest: HttpRequest): HttpResponse => {
-    const requiredField = ['name', 'email', 'password', 'passwordConfirmation']
+    const requiredField = ['name', 'email', 'password', 'passwordConfirmation', 'isProfessional', 'professionalName']
     for (const field of requiredField) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
