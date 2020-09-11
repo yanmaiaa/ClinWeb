ac139c774dba0b6b59db605bdee94f6940631c32
Joan Pedro <jpos.senai@gmail.com>
2020-09-11 12:23:12 -0300

    feat: ensure SignUpController returns 400 if isProfessional is not a number

--- backend/src/presentation/controllers/signup.ts
+++ backend/src/presentation/controllers/signup.ts
@@ -3,20 +3,23 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
+import { FieldValidator } from '../protocols/field-validator'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignUpController implements Controller {
-  constructor (private readonly emailValidator: EmailValidator) {}
+  constructor (
+    private readonly fieldValidator: FieldValidator,
+    private readonly emailValidator: EmailValidator
+  ) {}
 
   handle = (httpRequest: HttpRequest): HttpResponse => {
     const requiredField = ['name', 'email', 'password', 'passwordConfirmation', 'isProfessional', 'professionName']
     for (const field of requiredField) {
       if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
-      if (field === 'isProfessional' && typeof httpRequest.body[field] !== 'number') {
-        return badRequest(new InvalidParamError(field))
-      }
     }
     const isValid = this.emailValidator.isValid(httpRequest.body.email)
     if (!isValid) return badRequest(new InvalidParamError('email'))
+    const isNumber = this.fieldValidator.isNumber(httpRequest.body.isProfessional)
+    if (!isNumber) return badRequest(new InvalidParamError('isProfessional'))
   }
 }
---
+++ backend/src/presentation/protocols/field-validator.ts
@@ -0,0 +1,3 @@
+export interface FieldValidator {
+  isNumber?: (email: any) => boolean
+}
