4f5b5c4fc505fb0daf9abe0635cd014cbbdc0d86
Yan Maia <yandamasceno01@gmail.com>
2020-09-10 22:01:06 -0300

    Projeto Clin

---
+++ src/Components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styles from './Footer.module.css';
+
+const Footer = () => {
+  return <div className={Styles.footer}>Footer</div>;
+};
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Styles from './Header.module.css';
+
+const Header = () => {
+  return <div className={Styles.header}>Header</div>;
+};
+
+export default Header;
