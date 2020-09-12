d63bb24603cec6d76ab76c04679056761fe73f6a
Joan Pedro <jpos.senai@gmail.com>
2020-09-12 13:52:09 -0300

    feat: ensure BooleanValidatorAdapter returns true if validator returns true

--- backend/src/utils/boolean-validator-adapter.ts
+++ backend/src/utils/boolean-validator-adapter.ts
@@ -1,7 +1,8 @@
 import { BooleanValidator } from '../presentation/protocols'
+import validator from 'validator'
 
 export class BooleanValidatorAdapter implements BooleanValidator {
   isBoolean = (value: string): boolean => {
-    return false
+    return validator.isBoolean(value)
   }
 }
