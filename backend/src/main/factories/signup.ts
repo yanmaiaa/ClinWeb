5641e2b657dd0f148749a4ede05c3e1487333d3a
Joan Pedro <jpos.senai@gmail.com>
2020-09-16 12:49:14 -0300

    feat: add SignUp Factory

---
+++ backend/src/main/factories/signup.ts
@@ -0,0 +1,16 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { BooleanValidatorAdapter } from '../../utils/boolean-validator-adapter'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+  const addAccountRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, addAccountRepository)
+  const booleanValidatorAdapter = new BooleanValidatorAdapter()
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  return new SignUpController(booleanValidatorAdapter, emailValidatorAdapter, dbAddAccount)
+}
