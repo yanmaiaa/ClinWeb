dabe228dd19870016adb8341c7effd31201d085c
Joan Pedro <jpos.senai@gmail.com>
2020-09-16 11:52:45 -0300

    refactor: group middlewares exports

--- backend/src/main/config/middlewares.ts
+++ backend/src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from 'express'
-import { bodyParser } from '../middlewares/body-parser'
-import { contentType } from '../middlewares/content-type'
-import { CORS } from '../middlewares/cors'
+import { bodyParser, contentType, CORS } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
